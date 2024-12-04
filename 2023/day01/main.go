c9367d53fe2d20b5a384633fc8319e49be6fc941
fexh10 <effehgi@gmail.com>
2024-12-04 21:04:25 +0100

    day 01 2023: refactor part1 and implemented part2 (inspired by axtrat)

--- 2023/day01/main.go
+++ 2023/day01/main.go
@@ -4,90 +4,11 @@ import (
 	"bufio"
 	. "fmt"
 	"os"
+	"regexp"
 	"strconv"
-	"strings"
 	"unicode"
+	"strings"
 )
-
-func parte2(lines []string) {
-	diz := map[string]string{"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
-	var primo, ultimo string
-	index := 0
-	var numeri []int
-
-	for _, line := range lines {
-
-		for number := range diz {
-			if strings.Contains(line, number) {
-				if strings.Index(line, number) <= index {
-					primo = diz[number]
-				} else {
-					ultimo = diz[number]
-				}
-				index = strings.Index(line, number)
-			}
-			for i, char := range line {
-				for j := 0; j < 9; j++ {
-					if char == rune('0'+j) {
-						if i <= index {
-							primo = string(char)
-						} else {
-							ultimo = string(char)
-						}
-						index = i
-					}
-				}
-			}
-		}
-
-		if ultimo == "" {
-			ultimo = primo
-		}
-		n, _ := strconv.Atoi(primo + ultimo)
-		numeri = append(numeri, n)
-		primo, ultimo = "", ""
-		index = 0
-	}
-	sum := 0
-
-	for _, n := range numeri {
-		sum += n
-	}
-	Println(numeri[0])
-}
-
-func findNumbers(lines []string) []string {
-	var first, last string
-	var numeri []string
-
-	for _, lin := range lines {
-		for _, char := range lin {
-			if unicode.IsDigit(char) {
-				last = string(char)
-				if first == "" {
-					first = string(char)
-				}
-			}
-		}
-	
-		temp := first + last
-		numeri = append(numeri, temp)
-		first, last = "", ""
-	}
-	return numeri
-}
-
-func parte1(lines []string) {
-	var sum int
-
-	numeri := findNumbers(lines)
-
-	for _, e := range numeri {
-		n, _ := strconv.Atoi(e)
-		sum += n
-	}
-	Println(sum)
-}
 
 func input() []string {
 	file, _ := os.Open("input.txt")
@@ -103,8 +24,44 @@ func input() []string {
 	return lines
 }
 
+func part2(lines []string) (sum int) {
+	spelled := map[string]int{"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
+	for _, line := range lines {
+		var first, last int
+		for i := 0; i < len(line); i++ {
+			if unicode.IsDigit(rune(line[i])) {
+				if first == 0 {
+					first = int(line[i] - '0')
+				}
+				last = int(line[i] - '0')
+			}
+			for k, v := range spelled {
+				if strings.HasPrefix(line[i:], k) {
+					if first == 0 {
+						first = v
+					}
+					last = v
+				}
+			}
+		}
+		sum += first*10 + last
+	}
+	return
+}
+
+func part1(lines []string) (sum int) {
+	re := regexp.MustCompile(`\d`)
+	for _, line := range lines {
+		matched := re.FindAllString(line, -1)
+		addDigits := matched[0] + matched[len(matched)-1]
+		n, _ := strconv.Atoi(addDigits)
+		sum += n
+	}
+	return
+}
+
 func main() {
 	var lines []string = input()
-	parte1(lines)
-	parte2(lines)
+	Println(part1(lines))
+	Println(part2(lines))
 }
